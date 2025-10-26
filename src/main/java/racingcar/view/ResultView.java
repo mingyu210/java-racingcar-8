8245a5d7314eeaf8e1c5969572d1fabb28ae0434
mingyu210 <baemingyu5@gamil.com>
2025-10-26 21:52:42 +0900

    feat(ResultView): 최종 우승자 출력 메시지 추가

--- src/main/java/racingcar/view/ResultView.java
+++ src/main/java/racingcar/view/ResultView.java
@@ -16,6 +16,6 @@ public class ResultView {
 
     public void printWinners(List<Car> winners) {
         String winnerNames = winners.stream().map(Car::getName).collect(Collectors.joining(", "));
-        System.out.println(winnerNames);
+        System.out.println("최종 우승자 : " + winnerNames);
     }
 }
