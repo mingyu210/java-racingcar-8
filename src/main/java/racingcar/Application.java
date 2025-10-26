fc058d4824501a6afb60e568c5c788e550cfb379
mingyu210 <baemingyu5@gamil.com>
2025-10-26 21:50:12 +0900

    feat(Application): 프로그램 진입점 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,15 @@
 package racingcar;
 
+import racingcar.controller.RacingGameController;
+import racingcar.view.InputView;
+import racingcar.view.ResultView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        ResultView resultView = new ResultView();
+        RacingGameController controller = new RacingGameController(inputView, resultView);
+        controller.start();
     }
 }
