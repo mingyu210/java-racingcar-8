0d7e1515d89ac1a91f7aee6fe39c7e17566871a8
mingyu210 <baemingyu5@gamil.com>
2025-10-26 21:08:29 +0900

    refactor(InputView): Scanner 대신 Console.readLine() 사용하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,16 +1,17 @@
 package racingcar.view;
 
-import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class InputView {
-    private final Scanner scanner = new Scanner(System.in);
 
     public List<String> inputCarNames() {
         System.out.println("자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = scanner.nextLine().trim();
+
+        String input = Console.readLine().trim();
 
         List<String> carNames = Arrays.asList(input.split(","));
 
@@ -20,7 +21,7 @@ public class InputView {
 
     public int inputTryCount(){
         System.out.println("시도할 횟수는 몇 회인가요?");
-        String input = scanner.nextLine().trim();
+        String input = Console.readLine().trim();
         int count = Integer.parseInt(input);
         return count;
     }
