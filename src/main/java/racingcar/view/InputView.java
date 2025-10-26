8b392a0eb18a58da20aa98a20f46376a461f1a89
mingyu210 <baemingyu5@gamil.com>
2025-10-26 22:04:25 +0900

    feat(InputView): 시도 횟수가 음수거나 숫자가 아닌 경우 예외 처리 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -40,7 +40,14 @@ public class InputView {
     public int inputTryCount(){
         System.out.println("시도할 횟수는 몇 회인가요?");
         String input = Console.readLine().trim();
-        int count = Integer.parseInt(input);
-        return count;
+        try{
+            int count = Integer.parseInt(input);
+            if(count <=0){
+                throw new IllegalArgumentException("시도 횟수는 1 이상의 정수여야 합니다.");
+            }
+            return count;
+        } catch(NumberFormatException e){
+            throw new IllegalArgumentException("숫자를 입력해야 합니다.");
+        }
     }
 }
